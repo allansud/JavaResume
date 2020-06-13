ace84ea44ba77721664fed803711720c0471c794
Allan Freitas <freitas_allan@hotmail.com>
2020-06-13 11:42:25 -0300

    alterações nos arquivos html

--- src/main/java/br/com/resume/configuration/WebConfig.java
+++ src/main/java/br/com/resume/configuration/WebConfig.java
@@ -2,12 +2,10 @@ package br.com.resume.configuration;
 
 import java.util.Locale;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,26 +13,22 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-   @Autowired
-   private WebApplicationContext context;
+   // @Autowired
+   // private WebApplicationContext context;
 
-   @Bean
-	public ServletContextTemplateResolver templateResolver() {
-		final ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(context.getServletContext());
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".html");
-		resolver.setTemplateMode(TemplateMode.HTML);
-		return resolver;
-   }
+   // @Bean
+	// public ServletContextTemplateResolver templateResolver() {
+	// 	final ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(context.getServletContext());
+	// 	resolver.setPrefix("/WEB-INF/views/");
+	// 	resolver.setSuffix(".html");
+	// 	resolver.setTemplateMode(TemplateMode.HTML);
+	// 	return resolver;
+   // }
 
    @Override
    public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -52,19 +46,19 @@ public class WebConfig implements WebMvcConfigurer {
                        "classpath:/static/fonts/");
    }
    
-   @Bean
-	public SpringTemplateEngine templateEngine() {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver());
-		return templateEngine;
-   }
+   // @Bean
+	// public SpringTemplateEngine templateEngine() {
+	// 	SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	// 	templateEngine.setTemplateResolver(templateResolver());
+	// 	return templateEngine;
+   // }
    
-   @Bean
-	public ThymeleafViewResolver viewResolver() {
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-		viewResolver.setTemplateEngine(templateEngine());
-		return viewResolver;
-	}
+   // @Bean
+	// public ThymeleafViewResolver viewResolver() {
+	// 	ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	// 	viewResolver.setTemplateEngine(templateEngine());
+	// 	return viewResolver;
+	// }
    
    @Bean
    public LocaleResolver localeResolver() {
